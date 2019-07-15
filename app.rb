8589f3618c863422cfe0f436aa3270c4b207d7a4
Vinh Luu <vinhlt3@gmail.com>
2019-07-15 08:10:38 +0700

    Init project

---
+++ app.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'pdf-reader'
+
+reader = PDF::Reader.new('TuDienMoussay.pdf')
+
+puts reader.pdf_version
+puts reader.info
+puts reader.metadata
+puts reader.page_count
+
+# CODE HERE
