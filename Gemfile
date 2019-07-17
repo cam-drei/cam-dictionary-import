1d04aa42799593aee5640e5c116f138d65b1a906
Dung Luu <dungluu@Dungs-MacBook-Pro.local>
2019-07-17 07:33:41 +0700

    read page to page

--- Gemfile
+++ Gemfile
@@ -1,5 +1,9 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
+
 ruby '2.6.3'
 gem 'pdf-reader'
+
+gem 'mongoid'
+gem 'bson_ext'
--- app.rb
+++ app.rb
@@ -2,11 +2,18 @@
 
 require 'pdf-reader'
 
-reader = PDF::Reader.new('TuDienMoussay.pdf')
+# reader = PDF::Reader.new('TuDienMoussay.pdf')
 
-puts reader.pdf_version
-puts reader.info
-puts reader.metadata
+# puts reader.pdf_version
+# puts reader.info
+# puts reader.metadata
 # puts reader.page_count
 
+reader.pages.each do |page|
+  puts page.text
+end
+
+
+
 # CODE HERE
+
