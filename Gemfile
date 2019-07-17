58ce5a2a7c03f21afb7d3afbe10fa45a0f5f3953
Vinh Luu <vinhlt3@gmail.com>
2019-07-17 08:12:32 +0700

    wip

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.3'
 gem 'pdf-reader'
+gem 'pry'
 
 gem 'mongoid'
 gem 'bson_ext'
--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'pry'
 require 'pdf-reader'
 
 # reader = PDF::Reader.new('TuDienMoussay.pdf')
@@ -9,11 +10,30 @@ require 'pdf-reader'
 # puts reader.metadata
 # puts reader.page_count
 
-reader.pages.each do |page|
-  puts page.text
-end
+# binding.pry
 
+# puts reader.pages.first
 
+# reader.pages.each do |page|
+#   puts page.text
+# end
+
+# paragraph = ""
+# paragraphs = []
+# reader.pages.each do |page|
+#   lines = page.text.scan(/^.+/)
+#   lines.each do |line|
+#     if line.length > 55
+#       paragraph += " #{line}"
+#     else
+#       paragraph += " #{line}"
+#       paragraphs << paragraph
+#       paragraph = ""
+#     end
+#   end
+# end
 
 # CODE HERE
 
+sentence = 'a-hei a_ hE [Cam M] hay, hoan hç ≠ bravo.'
+# insert sentence into mongod
