76f65dd97ceaefaa173836925a570e0a5837ebe9
Vinh Luu <vinhlt3@gmail.com>
2019-07-18 17:45:17 +0700

    Fix for run

--- app.rb
+++ app.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
-# require 'pry'
+require 'pry'
 require 'pdf-reader'
 require 'mongo'
-
 
 # puts reader.pdf_version
 # puts reader.info
@@ -18,68 +17,55 @@ require 'mongo'
 #   puts page.text
 # end
 
+reader = PDF::Reader.new('TuDienMoussay.pdf')
+page = reader.page(1)
 
-  reader = PDF::Reader.new('TuDienMoussay.pdf')
-  page     = reader.page(1)
-
-  paragraph = ""
-  paragraphs = []
-  # reader.pages.each do |page|
-    lines = page.text.scan(/^.+/)
-    lines.each do |line|
-      if line.length > 55
-        paragraph += " #{line}"
-      else
-        paragraph += " #{line}"
-        paragraphs << paragraph
-        paragraph = ""
-      end
-    end
-    # return paragraphs
-  # end
+paragraph = ''
+paragraphs = []
+# reader.pages.each do |page|
+lines = page.text.scan(/^.+/)
+lines.each do |line|
+  if line.length > 55
+    paragraph += " #{line}"
+  else
+    paragraph += " #{line}"
+    paragraphs << paragraph
+    paragraph = ''
+  end
+end
+# return paragraphs
+# end
 
 puts paragraphs[0]
 
 client = Mongo::Client.new(['127.0.0.1:27017'], database: 'import')
 dictionary = client[:dictionary]
 
-def build_document(paragraphs)
+def build_document(paragraphs, dictionary)
   puts paragraphs[1]
   # binding.pry
-  
-  for i in 3..paragraphs.size - 1
+
+  (3..paragraphs.size - 1).each do |i|
     puts paragraphs[2]
     groups = paragraphs[i].split(' [Cam M] ')
     chams = groups[0].split(' ', 2)
     meanings = groups[1].split('=', 2)
-    {
+    document = {
       rumi: chams[0],
       akharThrah: chams[1],
       source: 'Cam M',
       vietnamese: meanings[0],
       french: meanings[1]
     }
-    # puts paragraph
-    
-    dictionary.insert_one(paragraphs[i])
+
+    # binding.pry
+
+    puts 'document:', document
+    dictionary.insert_one(document)
   end
-  
 end
 
-build_document(paragraphs)
-
-
-
-
-
-
-
-
-
-
-
-
-
+build_document(paragraphs, dictionary)
 
 # CODE HERE
 
