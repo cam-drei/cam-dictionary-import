32a6db98a16a3583179387425629579096f113cc
Vinh Luu <vinhlt3@gmail.com>
2019-07-22 08:08:52 +0700

    Clean code

--- app.rb
+++ app.rb
@@ -10,69 +10,70 @@ require 'mongo'
 # puts reader.page_count
 
 reader = PDF::Reader.new('TuDienMoussay.pdf')
-# page = reader.page(1)
+page = reader.page(1)
 
 paragraph = ''
 paragraphs = []
-reader.pages.each do |page|
-  lines = page.text.scan(/^.+/)
-  lines.each do |line|
-    if line.length > 55
-      paragraph += " #{line}"
-    else
-      paragraph += " #{line}"
-      paragraphs << paragraph
-      paragraph = ''
-    end
+# reader.pages.each do |page|
+lines = page.text.scan(/^.+/)
+lines.each do |line|
+  if line.length > 55
+    paragraph += " #{line}"
+  else
+    paragraph += " #{line}"
+    paragraphs << paragraph
+    paragraph = ''
   end
 end
+# end
 
 # puts paragraphs[0]
 
 client = Mongo::Client.new(['127.0.0.1:27017'], database: 'import')
 dictionary = client[:dictionary]
 
+def build_fulfill_sentence(sentence)
+  return unless sentence.include?('[Cam M]') && sentence.include?('≠')
+
+  groups = sentence.split('[Cam M]')
+  chams = groups[0].split(' ', 2)
+  meanings = groups[1].split('≠', 2)
+
+  {
+    rumi: chams[0],
+    akharThrah: chams[1],
+    source: 'Cam M',
+    vietnamese: meanings[0],
+    french: meanings[1]
+  }
+end
+
+def build_french_meaning_only(_sentence)
+  return unless paragraphs[i].include?('[Cam M]')
+
+  groups = paragraphs[i].split('[Cam M]')
+  chams = groups[0].split(' ', 2)
+
+  {
+    rumi: chams[0],
+    akharThrah: chams[1],
+    source: 'Cam M',
+    vietnamese: nil,
+    french: groups[1]
+  }
+end
+
 def build_document(paragraphs, dictionary)
   # puts 'paragraphs 1', paragraphs[1]
   # binding.pry
   (0..paragraphs.size - 1 - 1).each do |i|
-    # puts "paragraphs #{i}", paragraphs[2]
-    if paragraphs[i].include?('[Cam M]') && paragraphs[i].include?('≠')
-      
-      groups = paragraphs[i].split('[Cam M]')
-      chams = groups[0].split(' ', 2)
-      meanings = groups[1].split('≠', 2)
-      document = {
-        rumi: chams[0],
-        akharThrah: chams[1],
-        source: 'Cam M',
-        vietnamese: meanings[0],
-        french: meanings[1]
-      }
-      # binding.pry
+    sentence = paragraphs[i]
+    document = build_fulfill_sentence(sentence)
+    document = build_french_meaning_only(sentence) if document
 
-      # puts "inside #{i}", document
-      dictionary.insert_one(document)
-    elsif paragraphs[i].include?('[Cam M]')
-      groups = paragraphs[i].split('[Cam M]')
-      chams = groups[0].split(' ', 2)
-      document = {
-        rumi: chams[0],
-        akharThrah: chams[1],
-        source: 'Cam M',
-        vietnamese: nil,
-        french: groups[1]
-      }
-      
-      dictionary.insert_one(document)
-      
-    else
-      # puts "file #{i}", paragraphs[i]
-      # binding.pry
-      File.open('other_document.txt', 'a') {|file| file.write(paragraphs[i] + "\n")}
-    end
+    dictionary.insert_one(document) if document
+    File.open('other_document.txt', 'a') { |file| file.write(sentence + "\n") } unless document
   end
 end
 
 build_document(paragraphs, dictionary)
-
