10634d012d9e258192229af1b6d114297a0482fb
Dung Luu <dung.luu07@gmail.com>
2019-07-23 17:11:10 +0700

    fix case two cham wold in sentence

--- app.rb
+++ app.rb
@@ -12,7 +12,8 @@ class ImportPDF
     @reader = PDF::Reader.new('TuDienMoussay.pdf')
     client = Mongo::Client.new(['127.0.0.1:27017'], database: 'import')
     @dictionary = client[:dictionary]
-
+    
+    dictionary.delete_many() if dictionary.find()
     File.delete(UNIMPORT_FILE_NAME) if File.exist?(UNIMPORT_FILE_NAME)
   end
 
@@ -21,8 +22,13 @@ class ImportPDF
     # binding.pry
     (0..paragraphs.size - 1).each do |i|
       sentence = paragraphs[i]
-      document = build_fulfill_sentence(sentence)
-      document = build_french_meaning_only(sentence) if document
+      count_cham_world = count_cham_world(sentence)
+      
+      
+      document = build_fulfill_one_cham_world(sentence) if count_cham_world == 2 || count_cham_world == 3
+      document = build_fulfill_two_cham_world(sentence) if count_cham_world == 4
+      document = build_french_meaning_only(sentence) if document.nil?
+      document = build_error_page1(sentence) if document.nil?
 
       dictionary.insert_one(document) if document
 
@@ -38,42 +44,88 @@ class ImportPDF
     paragraph = ''
     @paragraphs = []
 
-    page = reader.page(1)
-    # reader.pages.each do |page|
-    lines = page.text.scan(/^.+/)
-    lines.each do |line|
-      if line.length > 55
-        paragraph += " #{line}"
-      else
-        paragraph += " #{line}"
-        @paragraphs << paragraph
-        paragraph = ''
+    # page = reader.page(1)
+    reader.pages[0...20].each do |page|
+      lines = page.text.scan(/^.+/)
+      lines.each do |line|
+        if line.length > 55
+          paragraph += " #{line}"
+        else
+          paragraph += " #{line}"
+          @paragraphs << paragraph
+          paragraph = ''
+        end
       end
     end
-
     @paragraphs
   end
 
-  def build_fulfill_sentence(sentence)
-    return unless sentence.include?('[Cam M]') && sentence.include?('≠')
+  def count_cham_world(sentence)
+    if sentence.include?(' [Cam M] ')
+      groups = sentence.split(' [Cam M] ', 2)
+      chams = groups[0].split()
+      puts count_world = chams.count
+    elsif sentence.include?(' [Cam M')
+      groups = sentence.split(' [Cam M', 2)
+      chams = groups[0].split()
+      puts count_world = chams.count
+    end
+    count_world
+  end
 
-    groups = sentence.split('[Cam M]')
-    chams = groups[0].split(' ', 2)
+  def build_fulfill_two_cham_world(sentence)
+    # puts 'count 4'
+    return unless sentence.include?(' [Cam M] ') && sentence.include?('≠')
+
+    groups = sentence.split(' [Cam M] ', 2)
+    chams = groups[0].split(' ')
     meanings = groups[1].split('≠', 2)
 
     {
-      rumi: chams[0],
-      akharThrah: chams[1],
+      rumi: chams[0, 2].join(' '),
+      akharThrah: chams[2, 2].join(' '),
       source: 'Cam M',
       vietnamese: meanings[0],
       french: meanings[1]
     }
   end
 
-  def build_french_meaning_only(sentence)
-    return unless sentence.include?('[Cam M]')
+  def build_fulfill_one_cham_world(sentence)
+    # puts 'count 2'
+    return unless sentence.include?(' [Cam M] ') && sentence.include?('≠')
 
-    groups = sentence.split('[Cam M]')
+    groups = sentence.split(' [Cam M] ', 2)
+    chams = groups[0].split(' ', 2)
+    meanings = groups[1].split('≠', 2)
+
+    {
+      rumi: chams[0],
+      akharThrah: chams[1],  
+      source: 'Cam M',
+      vietnamese: meanings[0], 
+      french: meanings[1]
+    }
+  end
+
+  def build_french_meaning_only(sentence)
+    return unless sentence.include?(' [Cam M] ')
+
+    groups = sentence.split(' [Cam M] ', 2)
+    chams = groups[0].split(' ', 2)
+
+    {
+      rumi: chams[0],
+      akharThrah: chams[1],
+      source: 'Cam M',
+      vietnamese: nil,
+      french: groups[1]
+    }
+  end
+
+  def build_error_page1(sentence) # One world is error in page 1
+    return unless sentence.include?('[Cam M')
+
+    groups = sentence.split('[Cam M', 2)
     chams = groups[0].split(' ', 2)
 
     {
@@ -87,3 +139,8 @@ class ImportPDF
 end
 
 ImportPDF.new.import
+
+
+# .gsub(/\s+$/m, '') remove any sequence of white spaces at the end of the string
+# .gsub(/^\s+|\s+$/m, '')  remove any sequence of white space at the beginning and at the end of the string
+# .gsub(/^\s+/m, '')  remove any sequence of white spaces at the beginning of the string
